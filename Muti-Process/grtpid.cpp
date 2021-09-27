9b518a4b124ea26b0eb332bbd698492ea4aa6e59
Panor520 <linpan5065328@foxmail.com>
2021-09-27 08:21:12 +0800

    clear up CPP repositort

--- Muti-Process/grtpid.cpp
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <cstdio>
-int main()
-{
-	printf("本进程编号为：%d\n",getpid());
-	sleep(10);
-	return 0;
-}
--- SelfDesignedDemo/CplusplusDemoOne/Filestream.cpp
+++
@@ -1,8 +0,0 @@
-#include <fstream>
-//文件类型包含两种：
-//1.文本文件：以文本的ASCII码形式存储在计算机中
-//2.二进制文件：以文本的二进制形式存储在计算机中，用户一般不直接读取
-/*操作文件三大类：
-1.ofstream 写操作 o- output f- file
-2.ifstream 读操作 i- input
-3.fstream：读写操作*/
